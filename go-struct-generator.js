66bc2b7b24089493ccfa41743723ed1e7ce5a82f
Jaxon Pickett <jaxon_pickett1@homedepot.com>
2016-05-09 16:10:11 -0400

    Add code comments

--- go-struct-generator.js
+++ go-struct-generator.js
@@ -6,42 +6,52 @@ fs = require('fs');
 
 files = fs.readdirSync('json_objects');
 
+// Array to hold child structs
 var children = [];
 
 for (i in files) {
 
+	// Ignore non-conforming file names
 	if (!/([^\.]*)\.json/.test(files[i])){
 		continue
 	}
-	
+
+	// The parent struct is named after the file, TitleCased
 	var structName = /([^\.]*)\.json/.exec(files[i])[1];
 	structName = structName.capFirst();
 
+	// Parse file into serialized object. It better be valid JSON!
 	var curJSON = require('./json_objects/' + files[i]);
-	
+
+	// Start the struct def with a header
 	var structDef = 'type ' + structName + ' struct {\n';
 
+	// Append field definitions to the struct def, recursively
 	for (prop in curJSON){
 
 		structDef += renderField(prop, curJSON[prop]);
 	}
 
+	// Close the struct def
 	structDef += '}';
 
+	// Output
 	console.log(structDef);
 }
 
+// Output any child structs that were discovered
 for (child in children) {
 
 	console.log(children[child] + '}');
 
 }
 
+// Generate field definitions, recursively
 function renderField(key, value){
 
 	propType = typeof value;
-	// console.log(propType);
 
+	// Primitive types close the recursion loop
 	if (propType == "string") {
 		return '\t' + key.capFirst() + ' string `json:"'+ key + '"`' + '\n';
 	}else if (propType == "number"){
@@ -49,53 +59,70 @@ function renderField(key, value){
 	}else if (propType == "boolean"){
 		return '\t' + key.capFirst() + ' bool `json:"'+ key + '"`' + '\n';
 	}else if (propType == "object"){
+	// Objects must be further recursed.
 
-		// console.log(parseInt(key, 10));
-
+		// Arrays are a special case
 		if(Array.isArray(value)){
 
+			// We have to discover the type of the array elements.
+			// We only check element 0 so they better all be the same.
+			// This is a recursive function and will recurse until the element type is 
+			// no longer an array, recursively.
 			structType = getChildType(value[0]);
 
+			// This is a flag signifying the the element type is an object, which
+			// needs to be discovered. 
 			if(structType == ''){
+
 				structType = key.capFirst() + 'Struct'
 
+				// Start a new child struct def
 				child = 'type ' + structType + ' struct {\n';
 
+				// Walk through child properties until we get to the primitive types
 				for (subProp in value[0]) {
 					child += renderField(subProp, value[0][subProp]);
 				}
 
+				// We only keep track of unique child structs
 				if(children.indexOf(child) == -1){
 					children.push(child);
 				}
 
 			}
 
+			// returns the field definition
 			return '\t' + key.capFirst() + ' []' + structType + ' `json:"'+ key + '"`' + '\n';
 
 		}else{
-
+			
+			// Start a new child struct def
 			child = 'type ' + key.capFirst() + 'Struct struct {\n';
 
+			// Walk through child properties until we get to the primitive types
 			for (subProp in value) {
 				child += renderField(subProp, value[subProp]);
 			}
 
+			// We only keep track of unique child structs
 			if(children.indexOf(child) == -1){
 				children.push(child);
 			}
 
+			// return the field definition
 			return '\t' + key.capFirst() + ' ' + key.capFirst() +'Struct ' + ' `json:"'+ key + '"`' + '\n';
 		}
 	}
 }
 
+// A recursive function that walks through nested arrays until it gets to an object def or primitive type.
 function getChildType(value){
 	if (typeof value == 'string'){
 		return 'string';
 	}else if(typeof value == 'number'){
 		return 'float64';
 	}else if(Array.isArray(value)){
+		// Keep appending '[]' until we get to the bottom of the array nest.
 		return '[]' + getChildType(value[0]);
 	}else{
 		return '';
